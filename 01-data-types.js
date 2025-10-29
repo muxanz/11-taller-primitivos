96514e5f7a3e3e22f89a6cba72c9c2f9033d28cd
muxanz <muxanzz@gmail.com>
2025-10-29 15:37:49 -0500

    01-data-types.js is added

---
+++ 01-data-types.js
@@ -0,0 +1,13 @@
+let userName = "John Doe";
+let age = 29;
+let isStudent = true;
+
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+
+let noValue;
+let nullValue = null;
+
+console.log(noValue);
+console.log(nullValue);
